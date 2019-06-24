698ca7f2365607369d706da5fc22c193f499e442
Adam Jimenez <adam.jimenez@gmail.com>
2019-06-24 23:49:17 +0100

    server update count

--- src/views/servers/Summary.vue
+++ src/views/servers/Summary.vue
@@ -119,6 +119,17 @@
       <v-list two-line>                
         <v-list-tile>
             <v-list-tile-content>
+              <v-list-tile-title v-html="`Updates`"></v-list-tile-title>
+              <v-list-tile-sub-title>
+                {{data.updates}} updates ({{data.security_updates}} security updates)
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+
+      <v-list two-line>                
+        <v-list-tile>
+            <v-list-tile-content>
               <v-list-tile-title v-html="`Hostname`"></v-list-tile-title>
               <v-list-tile-sub-title>
                 {{data.hostname}}
