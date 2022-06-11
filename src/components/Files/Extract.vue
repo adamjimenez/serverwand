2fd76ee367b6ec3e4d09f6814d2e98db651c4891
Adam Jimenez <adam.jimenez@gmail.com>
2022-06-11 19:33:09 +0100

    extract progress

--- src/components/Files/Extract.vue
+++ src/components/Files/Extract.vue
@@ -1,12 +1,25 @@
 <template>
-  <v-col
-    class="flex-grow-0"
-    v-if="selected.length === 1 && fileExtension(selected[0].name) === 'zip'"
-  >
-    <v-btn @click="extract()" :disabled="!selected" title="Extract">
-      <v-icon>mdi-folder-zip-outline</v-icon>
-    </v-btn>
-  </v-col>
+  <div>    
+
+    <v-alert
+      v-if="msg"
+      dark
+      style="position: fixed; width: 100%; z-index: 10; left: 0; bottom: 0; margin: 0 auto;"
+      type="info"
+    >
+      {{ msg }}
+    </v-alert>
+
+    <v-col
+      class="flex-grow-0"
+      v-if="selected.length === 1 && ['zip', 'gz', 'tar'].indexOf(fileExtension(selected[0].name)) !== -1"
+    >
+      <v-btn @click="extract()" :disabled="!selected" title="Extract">
+        <v-icon>mdi-folder-zip-outline</v-icon>
+      </v-btn>
+    </v-col>
+
+  </div>
 </template>
 
 <script>
@@ -33,8 +46,9 @@ export default {
       data: {
         recursive: false,
       },
-      error: "",
+      error: '',
       dialog: false,
+      msg: ''
     };
   },
 
@@ -48,32 +62,18 @@ export default {
       var self = this;
       this.$emit("loading", true);
 
-      api
-        .post("servers/" + this.serverId + "/files", {
-          cmd: "extract",
-          path: this.path,
-          file: this.selected[0].name,
-        })
-        .then(function (response) {
-          console.log(response);
+      api.event(
+        'servers/' + this.serverId + '/files?cmd=extract&id=' + encodeURIComponent(this.selected[0].id),
+        result => {
+          self.msg = result.msg;
+        },
+        error => self.$emit("error", error),
+        () => {
+          self.msg = '';
+          self.$emit("complete");
+        }
+      );
 
-          if (response.data.success) {
-            self.dialog = false;
-            self.$emit("complete");
-          } else {
-            var error = response.data.error
-              ? response.data.error
-              : response.data;
-
-            self.$emit("error", error);
-          }
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-        .finally(function () {
-          this.$emit("loading", false);
-        });
     },
   },
 };
