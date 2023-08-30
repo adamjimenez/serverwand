e963a7f5ee8e4cd3809b705ae3c75d0756a3e803
Adam Jimenez <adam.jimenez@gmail.com>
2023-08-30 22:53:32 +0100

    fix

--- src/components/CloudBackups/Snapshot.vue
+++ src/components/CloudBackups/Snapshot.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-btn @click="newSnapshot" title="Create snapshot" icon>
-        <v-icon>mdi:mdi-cloud-upload</v-icon>
+      <v-icon>mdi:mdi-cloud-upload</v-icon>
     </v-btn>
 
     <v-dialog app v-model="dialog">
@@ -11,11 +11,7 @@
         <v-card-text>
           <v-text-field v-model="name" label="Name" autofocus></v-text-field>
 
-          <v-btn
-            color="primary"
-            @click="createSnapshot"
-            :disabled="name.length === 0"
-          >
+          <v-btn color="primary" @click="createSnapshot" :disabled="name.length === 0">
             Create snapshot
           </v-btn>
         </v-card-text>
@@ -27,30 +23,23 @@
         <v-card-title> Response </v-card-title>
 
         <v-card-text id="messageBody">
-          <v-textarea
-            :value="message"
-            readonly
-            auto-grow
-            :loading="fetching"
-          ></v-textarea>
+          <v-textarea :value="message" readonly auto-grow :loading="fetching"></v-textarea>
         </v-card-text>
 
         <v-divider></v-divider>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn
-            @click="
-              $emit('closed');
-              showMessage = false;
-            "
-            :disabled="fetching"
-          >
+          <v-btn @click="
+            $emit('closed');
+          showMessage = false;
+          " :disabled="fetching">
             Close
           </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+
   </div>
 </template>
 
@@ -78,37 +67,33 @@ export default {
     },
 
     createSnapshot() {
-      this.$confirm("Create snapshot?").then((res) => {
-        if (res) {
-          var self = this;
-          this.fetching = true;
+      var self = this;
+      this.fetching = true;
 
-          api
-            .post("servers/" + this.serverId + "/backups", {
-              cmd: 'snapshot',
-              name: this.name
-            })
-            .then(response => {
-                console.log(response);
+      api
+        .post("servers/" + this.serverId + "/backups", {
+          cmd: 'snapshot',
+          name: this.name
+        })
+        .then(response => {
+          console.log(response);
 
-                self.dialog=false;
+          self.dialog = false;
 
-                if(response.data.success) {
-                  self.$emit("complete", response.data.result);
-                } else {
-                  var error=response.data.error
-                    ? response.data.error
-                    :response.data;
+          if (response.data.success) {
+            self.$emit("complete", response.data.result);
+          } else {
+            var error = response.data.error
+              ? response.data.error
+              : response.data;
 
-                  self.$emit("error", error);
-                }
-              })
-            .catch((error) => console.log(error))
-            .finally(() => self.fetching = false);
+            self.$emit("error", error);
+          }
+        })
+        .catch((error) => console.log(error))
+        .finally(() => self.fetching = false);
 
-        }
-      });
-    },
+    }
   },
 };
 </script>
