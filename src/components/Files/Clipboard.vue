5852a6b2eff61f0ef83b4c230c0a43cc684c8a09
Adam Jimenez <adam.jimenez@gmail.com>
2023-10-12 20:01:58 +0100

    refactor

--- src/components/Files/Clipboard.vue
+++ src/components/Files/Clipboard.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <v-btn v-if="selected.length" @click="cut()" title="Cut" icon="mdi:mdi-content-cut"></v-btn>
-    <v-btn v-if="selected.length" @click="copy()" title="Copy" icon="mdi:mdi-content-copy">    </v-btn>
-    <v-btn v-if="data.items.length && path != data.src" @click="paste()" title="Paste" icon="mdi:mdi-content-paste">    </v-btn>
+    <v-btn v-if="selected.length" @click="copy()" title="Copy" icon="mdi:mdi-content-copy"> </v-btn>
+    <v-btn v-if="data.items.length && path != data.src" @click="paste()" title="Paste" icon="mdi:mdi-content-paste">
+    </v-btn>
     <Confirm ref="confirm" />
   </div>
 </template>
@@ -56,47 +57,44 @@ export default {
       this.selected.forEach(element => this.data.items.push(element.name));
     },
 
-    paste: async function() {
+    paste: async function () {
       console.log(this.data);
 
       var cmd = this.data.cut ? "Move" : "Copy";
 
-      if (
-        await this.$refs.confirm.open(
-          "Confirm",
-          cmd + " " + this.data.items.length + " items?"
-        )
-      ) {
-        var self = this;
-        this.fetching = true;
-
-        api
-          .post("servers/" + this.serverId + "/files", {
-            cmd: cmd.toLowerCase(),
-            path: this.path,
-            src: this.data.src,
-            files: this.data.items,
-          })
-          .then(response => {
-            console.log(response);
-
-            if (cmd.toLowerCase() === "move") {
-              self.data.items = [];
-            }
-
-            if (response.data.success) {
-              self.$emit("complete");
-            } else {
-              var error = response.data.error
-                ? response.data.error
-                : response.data;
-
-              self.$emit('error', error);
-            }
-          })
-          .catch(error => console.log(error))
-          .finally(() => self.fetching = false);
+      if (!await this.$refs.confirm.open(cmd + " " + this.data.items.length + " items?")) {
+        return;
       }
+
+      var self = this;
+      this.fetching = true;
+
+      api
+        .post("servers/" + this.serverId + "/files", {
+          cmd: cmd.toLowerCase(),
+          path: this.path,
+          src: this.data.src,
+          files: this.data.items,
+        })
+        .then(response => {
+          console.log(response);
+
+          if (cmd.toLowerCase() === "move") {
+            self.data.items = [];
+          }
+
+          if (response.data.success) {
+            self.$emit("complete");
+          } else {
+            var error = response.data.error
+              ? response.data.error
+              : response.data;
+
+            self.$emit('error', error);
+          }
+        })
+        .catch(error => console.log(error))
+        .finally(() => self.fetching = false);
     },
   },
 };
