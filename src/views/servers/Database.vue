10197c4d270b05602e113177273df9bd8d524853
Adam Jimenez <adam.jimenez@gmail.com>
2023-12-19 15:09:47 +0000

    layout tweaks

--- src/views/servers/Database.vue
+++ src/views/servers/Database.vue
@@ -5,56 +5,41 @@
     <Loading :value="loading" />
 
     <v-card :loading="fetching">
+      <v-card-actions>
+        <form :action="'http://' + data.ip + '/phpmyadmin/'" method="post" target="_blank">
+          <input type="hidden" name="pma_username" :value="data.db_name" />
+          <v-btn type="submit">
+            PhpMyAdmin &nbsp;
+            <v-icon right>mdi:mdi-open-in-new</v-icon>
+          </v-btn>
+        </form>
+      </v-card-actions>
 
-      <v-container class="ma-0">
-        <v-row>
-          <v-col cols="12" sm="6" md="4">
-            <v-card>
-                <v-card-text>
-                  <v-row>
-                    <v-col style="max-width: 40px;">
-                      <v-icon>fas fa-network-wired</v-icon>
-                    </v-col>
+      <IPRestrictions :active="data.ip_restrictions.active" :items="data.ip_restrictions.ips"
+        :path="'servers/' + serverId + '/database/iprestrictions'" @save="fetchData(true)"
+        :remoteAddr="data.remote_addr" />
 
-                    <v-col>Max Connection Usage</v-col>
-                  </v-row>
+      <v-card max-width="300">
+        <v-card-text>
+          <v-row>
+            <v-col style="max-width: 40px;">
+              <v-icon>fas fa-network-wired</v-icon>
+            </v-col>
+            <v-col>Max Connection Usage</v-col>
+          </v-row>
 
-                  <div class="pt-3" style="clear: both">
-                    <v-list-item-title>
-                      <v-progress-linear :model-value="(data.max_used_connections / data.max_connections) * 100"
-                        height="20" color="primary"></v-progress-linear>
-                    </v-list-item-title>
-                    <v-list-item-subtitle>
-                      {{ (data.max_used_connections) }} used of
-                      {{ (data.max_connections) }}
-                    </v-list-item-subtitle>
-                  </div>
-                </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-
-      <v-container fluid>
-        <v-row>
-          <v-col>
-            <form :action="'http://' + data.ip + '/phpmyadmin/'" method="post" target="_blank">
-              <input type="hidden" name="pma_username" :value="data.db_name" />
-              <v-btn type="submit">
-                PhpMyAdmin &nbsp;
-                <v-icon right>mdi:mdi-open-in-new</v-icon>
-              </v-btn>
-            </form>
-          </v-col>
-        </v-row>
-
-        <v-row>
-          <v-col>
-            <IPRestrictions :active="data.ip_restrictions.active" :items="data.ip_restrictions.ips"
-              :path="'servers/' + serverId + '/database/iprestrictions'" @save="fetchData(true)" :remoteAddr="data.remote_addr" />
-          </v-col>
-        </v-row>
-      </v-container>
+          <div class="pt-3" style="clear: both">
+            <v-list-item-title>
+              <v-progress-linear :model-value="(data.max_used_connections / data.max_connections) * 100" height="20"
+                color="primary"></v-progress-linear>
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ (data.max_used_connections) }} used of
+              {{ (data.max_connections) }}
+            </v-list-item-subtitle>
+          </div>
+        </v-card-text>
+      </v-card>
 
     </v-card>
 
